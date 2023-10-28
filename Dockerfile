05756aeab8272487cd4994638b504657c22f6bbc
hemanth <hemanthtadikonda29@gmail.com>
2023-10-28 11:03:21 +0530

    Kubernetes

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,5 @@ COPY          run.sh /
 ENTRYPOINT    ["bash", "/run.sh"]
 
 
+
+
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for parameter in catalogue.dev.MONGO_URL,MONGO_URL catalogue.dev.DOCUMENTDB,DOCUMENTDB; do
+for parameter in $PARAMETERS; do
   ssm_parameter=$(echo $parameter | awk -F , '{print $1}')
   app_parameter=$(echo $parameter | awk -F , '{print $2}')
   echo ${app_parameter}=\"$(aws ssm get-parameter  --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g')\" >>/root/parameters
