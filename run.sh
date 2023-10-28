67c12511a280f0f6dc488f1bcb1d4d9b23c89687
hemanth <hemanthtadikonda29@gmail.com>
2023-10-28 10:02:40 +0530

    Kubernetes

--- run.sh
+++ run.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-for parameter in "catalogue.dev.MONGO_URL,MONGO_URL catalogue.dev.DOCUMENTDB,DOCUMENTDB";do
-  ssm_parameter =  $(echo parameter | awk -F , '{ print $1 }'
-  app_parameter =  $(echo parameter | awk -F , '{ print $1 }'
+for parameter in "catalogue.dev.MONGO_URL,MONGO_URL catalogue.dev.DOCUMENTDB,DOCUMENTDB"; do
+  ssm_parameter =  $(echo $parameter | awk -F , '{ print $1 }' )
+  app_parameter =  $(echo $parameter | awk -F , '{ print $2 }' )
 
-  echo $app_parameter=aws ssm get-parameter --name $ssm_parameter --with-decryption | jq .Parameter.value | sed -e 's/"//g' >>/common/parameter
+  echo $app_parameter=\"$(aws ssm get-parameter --name $ssm_parameter --with-decryption | jq .Parameter.value | sed -e 's/"//g')\" >>/root/parameter
 done
+
+##!/bin/bash
+#
+#for parameter in docdb.dev.master_username,MONGO_URL docdb.dev.master_username,DOCUMENTDB; do
+#  ssm_parameter=$(echo $parameter | awk -F , '{print $1}')
+#  app_parameter=$(echo $parameter | awk -F , '{print $2}')
+#  echo ${app_parameter}=\"$(aws ssm get-parameter  --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g')\" >>/root/parameters
+#done
 
 
 
